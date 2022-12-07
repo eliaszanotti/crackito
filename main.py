9dbb046e08f46c56b19816162110758eb8593730
Elias Zanotti <ezanotti@z2r1p7.42lyon.fr>
2022-12-07 21:02:54 +0100

    add main.py : first working

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time 
 
-driver = webdriver.Chrome("chromedriver")
-driver.get("https://open.spotify.com/")
+def f_accept_cookies(driver) :
+    time.sleep(1)
+    cookie_button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+    cookie_button.click()
 
-test = driver.find_element(By.CLASS_NAME, "IPVjkkhh06nan7aZK7Bx")
-test.click()
+def f_play_song(driver) :
+    time.sleep(5)
+    driver.find_element(By.CLASS_NAME, "futnNt").click()
 
+def main() :
+    url = "https://open.spotify.com"
+    driver = webdriver.Chrome("chromedriver")
+    driver.get(url)
+    cookie = {'name' : 'sp_dc', 'value' : 'AQDtiTt2-EjQDbHNrOh0NsLbZ_tSbmGPCqFc_5c2ORruTC1od0E9hMfmAEAJpX-w7o0aac4Hk-DDUIgFik7E3dujW10RkQtHJmG8hwOm_9OqZbPPMchjHWULbl8oAMVf9H-lWEgdVJLQ5I40n8MLM3Zc6mt9LOlz'}
+    driver.add_cookie(cookie)
+    f_accept_cookies(driver)
+    driver.get("{}/playlist/0WXMfN8PQKk9cFzkyhCUG2".format(url))
+    f_play_song(driver)
+    time.sleep(1000)
 
-
-time.sleep(1000)
-
-
-
-
-
-
+if __name__ == "__main__":
+   main()
