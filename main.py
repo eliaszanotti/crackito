aea6d12e95032f6ba1fe71743b7c3b1736676507
Bilal Zentout <bilalzentout@MacBook-Pro-de-Bilal.local>
2022-12-07 18:53:01 +0100

    main v1

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-test
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time 
+
+driver = webdriver.Chrome("chromedriver")
+driver.get("https://open.spotify.com/")
+
+test = driver.find_element(By.CLASS_NAME, "IPVjkkhh06nan7aZK7Bx")
+test.click()
+
+
+
+time.sleep(1000)
+
+
+
+
+
 
